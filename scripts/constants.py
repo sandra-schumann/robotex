57462c222e3a6cd378f20951dde95895ae10a3ae
Sandra Schumann <sandra.schumann426@gmail.com>
2017-11-06 15:30:15 +0200

    changed constants a bit

--- scripts/constants.py
+++ scripts/constants.py
@@ -5,6 +5,7 @@ goal_diameter = 16.0
 goal_height = 80.0
 field_width = 310.0
 field_length = 460.0
+robot_radius = 15.0
 
 h = 32.0
 a = 22.0
@@ -22,5 +23,5 @@ v = 0.2
 robot_ID = 'C'
 field_ID = 'B'
 
-robot_start_x = 310-15
-robot_start_y = 15
+robot_start_x = field_length - robot_radius
+robot_start_y = robot_radius
