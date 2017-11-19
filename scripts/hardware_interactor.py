3a9f9a411706d217d3cb559bbb5322aa218dd89d
Sandra Schumann <sandra.schumann426@gmail.com>
2017-11-19 11:23:01 +0200

    hardware interactor changes

--- scripts/hardware_interactor.py
+++ scripts/hardware_interactor.py
@@ -47,7 +47,7 @@ def __main__():
     pub = rospy.Publisher("FromRef", String, queue_size=10)
     pub2 = rospy.Publisher("FromMotors", Int32MultiArray, queue_size=10)
     init_mainboard()
-    rate = rospy.Rate(500)
+    rate = rospy.Rate(100)
     while not rospy.is_shutdown():
         c = read_ref_commands()
         if c:
