76476679b9069963ad7046251ecdda00e74b531a
Sandra Schumann <sandra.schumann426@gmail.com>
2017-11-20 09:11:18 +0200

    Simulator, pathfinding, magic, everything

--- scripts/talker.py
+++ scripts/talker.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import String, Int32
+from std_msgs.msg import String, Int32MultiArray
 from random import randint
 
 def talker():
-    pub = rospy.Publisher("chatter", Int32, queue_size=10)
+    pub = rospy.Publisher("GoTo", Int32MultiArray, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = randint(1,2)
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+        robot_pos = Int32MultiArray(data=[ int(x) for x in raw_input("Go to point: ").split() if x ])
+        #~ rospy.loginfo(robot_pos)
+        pub.publish(robot_pos)
+        #~ rate.sleep()
 
 if __name__ == '__main__':
     try:
