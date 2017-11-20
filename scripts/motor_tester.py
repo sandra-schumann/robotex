e5c9186e6ed936ba6f9bcbc3d0cb1f6425315f1f
Sandra Schumann <sandra.schumann426@gmail.com>
2017-11-20 13:00:05 +0200

    for crying out loud

--- scripts/motor_tester.py
+++ scripts/motor_tester.py
@@ -4,13 +4,20 @@ import rospy
 from std_msgs.msg import String, Int32MultiArray
 from random import randint
 
+def get_motor_speeds(ax, ay, omega):
+    f1 = ax*0.58 - ay*0.33 + omega*0.33
+    f2 = -ax*0.58 - ay*0.33 + omega*0.33
+    f3 = ay*0.67 + omega*0.33
+    return int(round(f1*128)), int(round(f2*128)), int(round(f3*128))
+
 def talker():
     pub = rospy.Publisher("ToMotors", Int32MultiArray, queue_size=10)
     rospy.init_node('motor_tester', anonymous=True)
     rate = rospy.Rate(0.5) # 10hz
     direction = 1
     while not rospy.is_shutdown():
-        dirs = Int32MultiArray(data=[3*direction, 3*direction, 3*direction])
+        goright = get_motor_speeds(0,0.1,0)
+        dirs = Int32MultiArray(data=goright)
         direction = direction*(-1)
         rospy.loginfo(dirs)
         pub.publish(dirs)
