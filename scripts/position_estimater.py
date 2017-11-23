fef5f60eb4a6169daea14402dafe042792d7f5c7
Sandra Schumann <sandra.schumann426@gmail.com>
2017-11-23 17:26:53 +0200

    just stuff

--- scripts/position_estimater.py
+++ scripts/position_estimater.py
@@ -130,16 +130,16 @@ def __main__():
                     tball_ang = balls_angle[0]
                     if tball_ang > 1*math.pi/180:
                         print "ball angle is", tball_ang, "greater than 1"
-                        omega = -max(min(0.3, tball_ang*180/math.pi/100),0.05)
+                        omega = -max(min(0.4, tball_ang*180/math.pi/100),0.05)
                     elif tball_ang < -1*math.pi/180:
                         print "ball angle is", tball_ang, "less than 1"
-                        omega = max(min(0.3, -tball_ang*180/math.pi/100),0.05)
+                        omega = max(min(0.4, -tball_ang*180/math.pi/100),0.05)
                     else:
                         omega = 0
                     if omega == 0:
-                        vx = max(min(0.2,(tball_dist-20)/20),0.05)
+                        vx = max(min(0.4,(tball_dist-20)/20),0.05)
                     else:
-                        vx = max(min(0.2,(tball_dist-20)/20),0.05)/3
+                        vx = max(min(0.4,(tball_dist-20)/20),0.05)/3
                     f1, f2, f3 = get_motor_speeds(vx, 0, omega)
                     pubmot.publish(Int32MultiArray(data=[f1, f2, f3]))
             
